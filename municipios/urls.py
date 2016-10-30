672df38738f7efe80a29285cade594cac7d01b5a
Sidnei Jr. Pereira <sidneifpj@gmail.com>
2016-10-30 13:22:39 -0200

    Update urls.py
    Atualizado para o django 1.10

--- municipios/urls.py
+++ municipios/urls.py
@@ -1,13 +1,14 @@
+# Alterado para o django 1.10
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
+    
+from municipios.views import base_url_js, municipios_ajax, teste
 
-
-urlpatterns = patterns(
-    'municipios.views',
-    url(r'^$', 'base_url_js', name='municipios-base-url'),
-    url(r'^base_url.js$', 'base_url_js', name='municipios-base-url-js'),
-    url(r'^ajax/municipios/(?P<uf>\w\w)/(?P<app_label>\w+)/(?P<object_name>\w+)/$', 'municipios_ajax', name='municipios-ajax'),
-    url(r'^teste/', 'teste', name='municipios-teste'),
-)
+urlpatterns = [
+    url(r'^$', base_url_js, name='municipios-base-url'),
+    url(r'^base_url.js$', base_url_js, name='municipios-base-url-js'),
+    url(r'^ajax/municipios/(?P<uf>\w\w)/(?P<app_label>\w+)/(?P<object_name>\w+)/$', municipios_ajax, name='municipios-ajax'),
+    url(r'^teste/', teste, name='municipios-teste'),
+]
